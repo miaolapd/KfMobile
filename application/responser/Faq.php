116d11da43a7a73e53c825f72628e40b8f5b10e6
miaolapd <miaolapd@163.com>
2020-11-29 16:28:16 +0800

    修正论坛帮助无法解析的问题

--- application/responser/Faq.php
+++ application/responser/Faq.php
@@ -28,11 +28,14 @@ class Faq extends Responser
         $commonData = array_merge($this->getCommonData($doc), $extraData);
         $matches = [];
 
-        $pqArea = pq('.kf_share1 > tr:last-child');
+        $faqId = $extraData['id'];
+
+        $pqArea = pq('.kf_gd1')->parent();
 
         // 问题列表
         $questionList = [];
-        foreach ($pqArea->find('> td:first-child > div > a') as $item) {
+        $questionTitle = '';
+        foreach ($pqArea->find('> .kf_gd1 > tr > td > a') as $item) {
             $pqItem = pq($item);
             $id = 0;
             $title = '';
@@ -40,16 +43,28 @@ class Faq extends Responser
                 $id = intval($matches[1]);
             }
             $title = trim_strip($pqItem->text());
+            if ($id == $faqId) {
+                $questionTitle = $title;
+            }
             $questionList[] = ['id' => $id, 'title' => $title];
         }
 
+
         // 问题内容
-        $pqContentCell = $pqArea->find('> td:last-child');
-        $questionTitle = trim_strip($pqContentCell->find('> div:first-child')->text());
-        $questionContent = replace_common_html_content($pqContentCell->find('> div:last-child')->html());
-        $questionContent = str_replace('=======================================', '======================', $questionContent);
+        $pqArea->find('> .kf_gd1:first')->remove();
+        $pqArea->find('> .line')->remove();
+        if ($pqArea->find('> .kf_gd2')->length > 0) {
+            $pqContentCell = $pqArea->find('> .kf_gd2 > tr > td');
+        }
+        else {
+            $pqContentCell = $pqArea;
+        }
+        $pqContentCell = $this->handleSpecialFaq($faqId, $pqContentCell);
+
+        $questionContent = replace_common_html_content($pqContentCell->html());
 
         $data = [
+            'faqId' => $faqId,
             'questionList' => $questionList,
             'questionTitle' => $questionTitle,
             'questionContent' => $questionContent,
@@ -58,5 +73,34 @@ class Faq extends Responser
         trace('phpQuery解析用时：' . debug('begin', 'end') . 's' . '（初始化：' . $initTime . 's）');
         if (config('app_debug')) trace('响应数据：' . json_encode($data, JSON_UNESCAPED_UNICODE));
         return array_merge($commonData, $data);
+    }
+
+    /**
+     * 处理特殊帮助内容
+     * @param string $faqId 当前帮助ID
+     * @param \phpQueryObject $pqContentCell
+     * @return \phpQueryObject 帮助内容节点
+     */
+    protected function handleSpecialFaq($faqId, $pqContentCell) {
+        // 处理评分奖励规则
+        if ($faqId == '102') {
+            $pqTable = $pqContentCell->find('> .kf_gd1');
+            $pqTable->addClass('table table-responsive table-bordered table-striped');
+
+            $pqTable->wrapInner('<tbody></tbody>');
+
+            $pqTableHeader = $pqTable->find('> tbody > tr:first-child');
+            $pqTableHeader->prependTo($pqTable);
+            $pqTableHeader->wrap('<thead class="thead-light faq-thead"></thead>');
+            $pqTableHeader->html(preg_replace('/\btd\b/', 'th', $pqTableHeader->html()));
+
+            foreach ($pqTable->find('> tbody > tr > td:first-child') as $item) {
+                $pqItem = pq($item);
+                $cellContent = $pqItem->text();
+                $pqItem->replaceWith('<th>' . $cellContent . '</th>');
+            }
+        }
+
+        return $pqContentCell;
     }
 }
